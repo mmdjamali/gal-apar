8defb7ae2d255f9d624d144c28b32e3bd8a634e7
MmD <mmdj27634@gmail.com>
2023-05-22 19:54:43 +0330

    fix : ts error for mongoClientPromise

--- src/lib/db-client.ts
+++ src/lib/db-client.ts
@@ -9,17 +9,20 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client : MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if ((!global._mongoClientPromise)) {
+
+  let globalWithMongoClientPromise = global as typeof globalThis & { _mongoClientPromise : Promise<MongoClient> }
+
+  if ((!globalWithMongoClientPromise._mongoClientPromise)) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    globalWithMongoClientPromise._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalWithMongoClientPromise._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
