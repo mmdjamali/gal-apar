185fa5eee839062416dbf6cc8c7e719a767b2ad8
MmD <mmdj27634@gmail.com>
2023-07-16 08:17:57 -0700

    add : some new icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -26,11 +26,13 @@ import {
   RiImageLine,
   RiLineChartLine,
   RiLoader5Line,
+  RiLoginBoxLine,
   RiLogoutBoxLine,
   RiMacbookLine,
   RiMoonFoggyLine,
   RiMoreLine,
   RiNotification3Line,
+  RiQuestionMark,
   RiSearchLine,
   RiSettingsLine,
   RiShakeHandsLine,
@@ -82,6 +84,8 @@ export const Icons: {
   ImageAdd: RiImageAddLine,
   Image: RiImageLine,
   ExpandY: RiExpandUpDownLine,
+  Login: RiLoginBoxLine,
+  QuestionMark: RiQuestionMark,
   Tooman: (props: IconBaseProps) => (
     <svg
       {...props}
@@ -176,8 +180,8 @@ export const Icons: {
         d="M9 4V13C9 13.5304 8.78929 14.0391 8.41421 14.4142C8.03914 14.7893 7.53043 15 7 15H6C5.20435 15 4.44129 14.6839 3.87868 14.1213C3.31607 13.5587 3 12.7956 3 12V11M12 5V13C12 13.2652 12.1054 13.5196 12.2929 13.7071C12.4804 13.8946 12.7348 14 13 14H14C14.5304 14 15.0391 13.7893 15.4142 13.4142C15.7893 13.0391 16 12.5304 16 12V11M21 14V15.096C20.9999 16.2109 20.6271 17.2938 19.9409 18.1725C19.2548 19.0513 18.2946 19.6755 17.213 19.946L17 20M11 18H11.01M14 18H14.01"
         stroke="currentColor"
         strokeWidth="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   ),
