f613fc4cfd0348238ce516378808049a41d04a47
MmD <mmdj27634@gmail.com>
2023-08-01 04:59:51 -0700

    feat(cart) : add types for profucts, varinats and cart products

--- types/product.ts
+++ types/product.ts
@@ -1,9 +1,18 @@
-type VariantType = {
+export type VariantType = {
   _id?: string;
-  price?: string | null;
+  price?: number | null;
   color?: string;
   quantity?: string;
   size?: string;
 };
 
-export { type VariantType };
+export type ProductType = {
+  name : string,
+  images : string[];
+  currency : string;
+  _id?: string;
+  price?: number | null;
+  base_prise : number;
+  quantity?: string;
+  variants : VariantType[] | string[] | null
+};
