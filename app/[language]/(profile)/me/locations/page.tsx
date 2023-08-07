4fe8b10ff4756ba588b9b41758bef552eed8462c
MmD <mmdj27634@gmail.com>
2023-08-07 03:05:38 -0700

    fix : remove test pick location component

--- app/[language]/(profile)/me/locations/page.tsx
+++ app/[language]/(profile)/me/locations/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const PickLocation = dynamic(
-  () => import("@/components/locations/pick-location"),
-  {
-    loading: () => <p>loading...</p>,
-    ssr: false,
-  }
-);
+// const PickLocation = dynamic(
+//   () => import("@/components/locations/pick-location"),
+//   {
+//     loading: () => <p>loading...</p>,
+//     ssr: false,
+//   }
+// );
 
 function Locations() {
   return (
@@ -15,7 +15,7 @@ function Locations() {
       <div className="flex flex-col w-full mb-6">
         <h1 className="text-[26px] font-bold">Locations</h1>
       </div>
-      <PickLocation />
+      {/* <PickLocation /> */}
     </div>
   );
 }
