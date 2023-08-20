9cd4fade5788d5eec1a454d73bec41caa20096de
MmD <mmdj27634@gmail.com>
2023-08-20 05:30:58 -0700

    ref : change image and link in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,15 +6,15 @@ const Footer = ({ language }: WithLanguageType) => {
   return (
     <footer className="container flex flex-col md:flex-row items-center justify-between gap-4 max-w-[1300px] mx-auto py-2">
       <Avatar className="h-8 w-8">
-        <AvatarImage src="https://github.com/1stMmD.png" />
+        <AvatarImage src="https://github.com/mmdjamali.png" />
         <AvatarFallback />
       </Avatar>
 
       {language === "fa" ? (
         <p className="font-medium md:ml-auto text-center">
           ساخته شده توسط{" "}
-          <a className="underline" href="https://github.com/1stMmD">
-            1stMmD
+          <a className="underline" href="https://github.com/mmdjamali">
+            mmdjamali
           </a>{" "}
           و دپلوی شده در{" "}
           <a className="underline" href="https://vercel.com">
@@ -24,8 +24,8 @@ const Footer = ({ language }: WithLanguageType) => {
       ) : (
         <p className="font-medium md:mr-auto text-center">
           Built by{" "}
-          <a className="underline" href="https://github.com/1stMmD">
-            1stMmD
+          <a className="underline" href="https://github.com/mmdjamali">
+            mmdjamali
           </a>{" "}
           and deployed on{" "}
           <a className="underline" href="https://vercel.com">
