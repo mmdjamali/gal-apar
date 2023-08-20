9f0be18847b2a81316f0926f80dccce9e26debd5
MmD <mmdj27634@gmail.com>
2023-08-20 05:35:42 -0700

    ref : add zod into adding location

--- pages/api/location/index.ts
+++ pages/api/location/index.ts
@@ -4,42 +4,61 @@ import { LocationModel } from "@/models/location";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
+import z from "zod";
+
+const z_location = z.object({
+  address: z.string(),
+  lat: z.number(),
+  lot: z.number(),
+  receiver_name: z.string(),
+  receiver_last_name: z.string(),
+  receiver_phone_number: z.string(),
+});
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await connectDB();
+    await connectDB();
 
-  if (req.method === "POST") {
-    const body = req?.body;
+    if (req.method === "POST") {
+      const {
+        address,
+        lat,
+        lot,
+        receiver_last_name,
+        receiver_name,
+        receiver_phone_number,
+      } = z_location.parse(req.body);
 
-    const user = (await getServerSession(req, res, nextAuthConfig))?.user;
+      const user = (await getServerSession(req, res, nextAuthConfig))?.user;
 
-    if (!user) return res.status(401).json("Auth required!");
+      if (!user) return res.status(401).json("Auth required!");
 
-    await LocationModel.create({
-      user: user._id,
-      address: body.address,
-      lat: body.lat,
-      lot: body.lot,
-      receiver_name: body.receiver_name,
-      receiver_last_name: body.receiver_last_name,
-      receiver_phone_number: body.receiver_phone_number,
-    });
+      await LocationModel.create({
+        user: user._id,
+        address,
+        lat,
+        lot,
+        receiver_name,
+        receiver_last_name,
+        receiver_phone_number,
+      });
 
-    const locations = await LocationModel.find({ user: user._id });
+      const locations = await LocationModel.find({ user: user._id });
 
-    return res.json(locations);
-  }
+      return res.json(locations);
+    }
 
-  if (req.method === "GET") {
-    const user = (await getServerSession(req, res, nextAuthConfig))?.user;
+    if (req.method === "GET") {
+      const user = (await getServerSession(req, res, nextAuthConfig))?.user;
 
-    if (!user) return res.status(401).json("Auth required!");
+      if (!user) return res.status(401).json("Auth required!");
 
-    const locations = await LocationModel.find({ user: user._id });
+      const locations = await LocationModel.find({ user: user._id });
 
-    return res.json(locations);
-  }
+      return res.json(locations);
+    }
 
-  return res.status(404).end();
+    return res.status(404).end();
+
 };
 
 export default handler;
