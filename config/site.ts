dd925d286889f068f332112155d40ec77cdeba27
MmD <mmdj27634@gmail.com>
2023-08-20 02:17:07 -0700

    feat : add items to config file

--- config/site.ts
+++ config/site.ts
@@ -1,3 +1,71 @@
+import { MainNavItem } from "@/types";
+
+const items: MainNavItem[] = [
+  {
+    title: "Lobby",
+    items: [
+      {
+        title: "test2",
+        description: "Lorem ipsum dolor sit",
+        url: "https://mmdjamali.ir",
+      },
+      {
+        title: "test1",
+        description: "Lorem ipsum dolor sit amet",
+        url: "https://mmdjamali.ir",
+      },
+      {
+        title: "test3",
+        description: "Lorem ipsum dolor sit",
+        url: "https://mmdjamali.ir",
+      },
+    ],
+  },
+  {
+    title: "Test",
+    items: [
+      {
+        title: "test1",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dignissimos repellat",
+        url: "https://mmdjamali.ir",
+      },
+      {
+        title: "test2",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dignissimos repellat",
+        url: "https://mmdjamali.ir",
+      },
+      {
+        title: "test3",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dignissimos repellat",
+        url: "https://mmdjamali.ir",
+      },
+      {
+        title: "test4",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dignissimos repellat",
+        url: "https://mmdjamali.ir",
+      },
+      {
+        title: "test5",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dignissimos repellat",
+        url: "https://mmdjamali.ir",
+      },
+      {
+        title: "test6",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dignissimos repellat",
+        url: "https://mmdjamali.ir",
+      },
+    ],
+  },
+];
+
 export const siteConfig = {
   name: "Gal apar",
+  description: "sjjjdjshds sdjdjskjdk kjdskjd",
+  items,
 };
